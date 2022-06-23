279d2b3925521eccaafbb331e3c5f2bd1f2bea61
johnmart19 <johnivan19999@gmail.com>
2022-06-24 02:48:41 +0300

    VoidUI: Add GameSpace to compilation targets
    Change-Id: I7c82f0b342915aca3e0aa2079c8cce2aa16efdd4

--- config/common.mk
+++ config/common.mk
@@ -116,6 +116,10 @@ PRODUCT_PACKAGES += \
     NoCutoutOverlay \
     DummyCutoutOverlay
 
+# GameSpace
+PRODUCT_PACKAGES += \
+    GameSpace
+
 # TouchGestures
 PRODUCT_PACKAGES += \
     TouchGestures
