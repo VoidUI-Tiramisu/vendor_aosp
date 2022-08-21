2abd778d502355e73227f9d5ee5c45b730681bc2
AnierinB <anierin@evolution-x.org>
2022-08-21 20:32:17 +0000

    vendorsetup: Use recursive find & replace to remove the ndk_platform backend
    - This is just a temporary change until externally tracked hals are updated to 13 with the ndk_platform removed.
    - when . build/evnsetup.sh is ran, it automatically executes this vendorsetup.sh script, thus ensuring the ndk_platform is removed from all applicable repos.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,8 @@ do
         lunch_others_targets+=("aosp_$device-$var")
     done
 done
+
+# Android 13 Automatic NDK_Platform changer
+# Replaces ndk_platform with ndk for SDK33 Environment
+(cd vendor/qcom && find . -type f -exec sed -i 's/ndk_platform/ndk/g' {} + )
+(cd hardware && find . -type f -exec sed -i 's/ndk_platform/ndk/g' {} + )
