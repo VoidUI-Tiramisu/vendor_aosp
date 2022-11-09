f161ff04497cc095c3ee479634b26ebb28394c32
johnmart19 <johnivan19999@gmail.com>
2022-11-09 05:56:29 +0200

    Revert "Changes for plus version"
    This reverts commit 72569390d944a11877827523a87009a325e0555e.

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -292,11 +292,7 @@ function githubremote()
         return 1
     fi
     git remote rm github 2> /dev/null
-    local REMOTE=$(git config --get remote.pixel-plus.projectname)
-    if [ -z "$REMOTE" ]
-    then
-        local REMOTE=$(git config --get remote.pixel.projectname)
-    fi
+    local REMOTE=$(git config --get remote.pixel.projectname)
 
     local PROJECT=$(echo $REMOTE | sed -e "s#platform/#android/#g; s#/#_#g")
 
@@ -407,18 +403,9 @@ function pixelgerrit() {
         return 1
     fi
     local user=`git config --get review.gerrit.pixelexperience.org.username`
-    local review=`git config --get remote.pixel-plus.review`
-    local remote_branch=thirteen-plus
-    if [ -z "$review" ]
-    then
-        local review=`git config --get remote.pixel.review`
-    fi
-    local project=`git config --get remote.pixel-plus.projectname`
-    if [ -z "$project" ]
-    then
-        local project=`git config --get remote.pixel.projectname`
-        local remote_branch=thirteen
-    fi
+    local review=`git config --get remote.pixel.review`
+    local project=`git config --get remote.pixel.projectname`
+    local remote_branch=thirteen
     local command=$1
     shift
     case $command in
@@ -659,11 +646,7 @@ function pixelrebase() {
         return
     fi
     cd $dir
-    repo=$(git config --get remote.pixel-plus.projectname)
-    if [ -z "$repo" ]
-    then
-        repo=$(git config --get remote.pixel.projectname)
-    fi
+    repo=$(git config --get remote.pixel.projectname)
     echo "Starting branch..."
     repo start tmprebase .
     echo "Bringing it up to date..."
